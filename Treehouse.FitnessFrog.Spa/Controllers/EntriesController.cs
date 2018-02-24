ddeee62a59f3f368f9448e593a8fbb26faf83d59
bhanu korremula <bkraider@gmail.com>
2018-02-24 01:37:35 -0500

    Added get, put, post and delete methods to the controller

--- Treehouse.FitnessFrog.Spa/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog.Spa/Controllers/EntriesController.cs
@@ -4,11 +4,42 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Models;
 
 namespace Treehouse.FitnessFrog.Spa.Controllers
 {
     public class EntriesController : ApiController
     {
-        //testing github commit 
+        public IEnumerable<Entry> Get()
+        {
+            var activityBiking = new Activity() { Name = "Biking" };
+
+            return new List<Entry>()
+            {
+                new Entry(2017, 1, 2, activityBiking, 10.0m),
+                new Entry(2017, 1, 3, activityBiking, 12.2m)
+            };
+        }
+
+
+        public Entry Get(int id )
+        {
+            return null;
+        }
+
+        public void Put()
+        {
+
+        }
+
+        public void Post()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
     }
 }
