4052e112680cade871f5e9d4fc88cd4abba74e5c
bhanu korremula <bkraider@gmail.com>
2018-02-26 14:00:55 -0500

    Updated the JSON Serializer to remove the self referencing loop

--- Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
+++ Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using Newtonsoft;
+using Newtonsoft.Json.Serialization;
 
 namespace Treehouse.FitnessFrog.Spa
 {
@@ -10,6 +12,14 @@ namespace Treehouse.FitnessFrog.Spa
     {
         public static void Register(HttpConfiguration config)
         {
+
+            var jsonSerializerSettings =
+                config.Formatters.JsonFormatter.SerializerSettings;
+            jsonSerializerSettings.ReferenceLoopHandling =
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            jsonSerializerSettings.ContractResolver = new
+                CamelCasePropertyNamesContractResolver();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
