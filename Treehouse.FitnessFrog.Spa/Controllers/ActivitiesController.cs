d142ef162a220eb754a7f6a542cdeddef05b605a
bhanu korremula <bkraider@gmail.com>
2018-02-27 11:34:22 -0500

    Added activities and intensities controllers

---
+++ Treehouse.FitnessFrog.Spa/Controllers/ActivitiesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Data;
+
+namespace Treehouse.FitnessFrog.Spa.Controllers
+{
+    public class ActivitiesController : ApiController
+    {
+        private ActivitiesRepository _activitiesRepository = null;
+
+        public ActivitiesController(ActivitiesRepository activitiesRepository)
+        {
+            _activitiesRepository = activitiesRepository;
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_activitiesRepository.GetList());
+        }
+    }
+}
